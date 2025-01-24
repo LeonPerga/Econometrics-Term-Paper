613bfcb7d9702076e62477e2393054961d20fbc6
Evya9999 <evyatarsegal1@gmail.com>
2025-01-24 12:22:40 +0200

    No need to filter data, we want to check offer not signup

--- project.R
+++ project.R
@@ -23,17 +23,9 @@ ssp_means
 control_means
 
 #P2Q3
-ssp_offer = filter(data, ssp_offer == 1)
-model1 = lm(ssp_signup ~ HS_GPA + age + female + english + dad_HS_grad + dad_college_grad 
-            + mom_HS_grad + mom_college_grad + uni_first_choice + finish_in_4_yrs + grad_degree + live_home + work_plans, ssp_offer)
+model1 = lm(ssp_offer ~ HS_GPA + age + female + english + dad_HS_grad + dad_college_grad 
+            + mom_HS_grad + mom_college_grad + uni_first_choice + finish_in_4_yrs + grad_degree + live_home + work_plans, data)
 coeftest(model1, vcov = vcovHC(model1, type = "HC1"))
-
-
-#correction for age
-ssp_offer_cor = filter(ssp_offer, age > 17 & age < 20)
-model1_cor = lm(ssp_signup ~ HS_GPA + age + female + english + dad_HS_grad + dad_college_grad 
-            + mom_HS_grad + mom_college_grad + uni_first_choice + finish_in_4_yrs + grad_degree + live_home + work_plans, ssp_offer_cor)
-coeftest(model1_cor, vcov = vcovHC(model1_cor, type = "HC1"))
 
 #P3Q4
 
